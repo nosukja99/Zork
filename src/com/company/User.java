9f88daa454cd6db6b45b9831881651a3edcf6a17
--konstantinGulish <tasso.stamboulis@gmail.com>
2018-06-07 11:35:09 -0400

    completed User class

--- src/com/company/User.java
+++ src/com/company/User.java
@@ -1,10 +1,31 @@
 package com.company;
 
+import java.util.HashMap;
+
 /* This is the user class, which will contain all variables and methods associated with user actions.*/
-abstract class User {
+class User {
     // constants:
+    final char [] directions = {'n','s','e','w'};
 
+    HashMap<Integer, Room> roomLookUp = new HashMap<>();
+
+    // constructors:
+     public User()
+     {
+         roomLookUp.put(1, new Foyer ());
+         roomLookUp.put(2, new FrontRoom ());
+         roomLookUp.put(3, new Library ());
+         roomLookUp.put(4, new Kitchen ());
+         roomLookUp.put(5, new Vault ());
+         roomLookUp.put(6, new Parlor ());
+         roomLookUp.put(7, new SecretRoom ());
+     }
     // instance variables:
 
-    // instance variables:
+    // instance methods:
+    public Room move ( Room currentRoom, char direction){
+        HashMap<Character, Integer> directions = currentRoom.getExit();
+        int roomNumber = directions.get(direction);
+        return roomLookUp.get(roomNumber);
+    }
 }
